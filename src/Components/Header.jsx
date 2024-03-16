f2421acb97e680bf4b527c9dbc97f93a3809da05
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-03-16 22:24:03 +0600

    create hrader page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,47 @@
 import { useState } from "react";
 import {
     FaSearch,
-    FaShoppingBag
+    FaShoppingBag,
+    FaSignInAlt,
+    FaUser
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+    const user= {_id:'afff',role:'admin'};
     const [isOpen, setIsOpen] = useState(false);
       return (
-   <nav className=" flex-row  text-cyan-400 justify-end align-middle gap-3">
+   <nav className=" flex flex-row justify-end align-center relative gap-6">
     <Link onClick={()=> setIsOpen(false)} to={'/'} >Home</Link>
     <Link onClick={()=> setIsOpen(false)} to={'/search'} > <FaSearch/> </Link>
     <Link onClick={()=> setIsOpen(false)} to={'/cart'} > <FaShoppingBag/> </Link>
+    {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog className="rounded-lg w-full text-end  absolute [left: calc(100% - 100px)] top-8  " open={isOpen}>
+            <div className="flex flex-col justify-end gap-2">
+              {user.role === "admin" && (
+                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                  Admin
+                </Link>
+              )}
+
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
+              <button className="text-end">
+               LogOut
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
    </nav>
   )
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import './Main.css';
 
 
 const Main = () => {
